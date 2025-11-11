39122f720bedbe26edd992b3d5d60d0a1cffc630
shahiddigilinkers02 <webdigilinkers@gmail.com>
2025-11-11 12:13:16 +0530

    Header & Footer created

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GIT Demo</title>
-</head>
-
-<body>
+<?php include_once('header.php') ?>
     <h1>This is git demo</h1>
-</body>
-
-</html>
+<?php include_once('footer.php') ?>
